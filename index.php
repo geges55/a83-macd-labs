6cabc0332744eb7ba81006f08b5d43e48106185c
Septian Geges <57484305+geges55@users.noreply.github.com>
2019-11-20 19:37:49 +0700

    Update index.php Submission 1
    Submission 1 Azure GGS

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "dicodingappserverggs.database.windows.net";
+    $user = "dicoding";
+    $pass = "123poiasD";
+    $db = "dicodingwebapp";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
